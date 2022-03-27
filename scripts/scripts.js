f53d8be76509e53841730ae89d53cc297bda3a04
Mark-NB <mark.nbstudios@gmail.com>
2022-03-27 23:50:38 +0200

    end of day 1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,88 @@
 const movieList = document.querySelector('.main__list');
+const radioBtns = Array.from(document.querySelectorAll('.nav__btn'));
 
-const addMoviesToDom = (movie) => {
-    movieList.appendChild(movie);
+
+const moviesToImg = (arg) => {
+    return arg.map((item => {
+        newImg = document.createElement("img");
+        newImg.src = item.poster;
+        newImg.id = item.imdbID;
+        return newImg;
+    }))
 };
 
 
-const movieLiArray = movies.map((item => {
-    newImg = document.createElement("img");
-    newImg.src = item.poster;
-    return newImg;
-}))
+const addMoviesToDom = (movie) => {
+    newLi = document.createElement('li');
+    movieList.appendChild(newLi);
+    newA = document.createElement('a');
+    newLi.appendChild(newA);
+    newA.href = `https://www.imdb.com/title/${movie.id}/`;
+    newA.appendChild(movie);
+};
 
-movieLiArray.forEach(addMoviesToDom);
+const clearMoviesinDom = () => {
+    currentList = Array.from(document.getElementsByTagName('li', 'img', 'a'));
+    currentList.forEach(item => {
+        movieList.removeChild(item);
+    })
+};
 
-console.log(movieLiArray);
+const filterMovies = (wordInMovie) => {
+    clearMoviesinDom();
+    const filterList = movies.filter(item => {
+        return item.title.includes(wordInMovie);
+    })
+    let filterListLi = moviesToImg(filterList);
+    filterListLi.forEach(item => {
+        addMoviesToDom(item);
+    })
+};
 
-//movieLiArray.forEach(addMoviesToDom);
+const filterLatestMovies = () => {
+    clearMoviesinDom();
+    const filterList = movies.filter(item => {
+        return item.year >= 2014;
+    })
+    let filterListLi = moviesToImg(filterList);
+    filterListLi.forEach(item => {
+        addMoviesToDom(item);
+    })
+};
 
+const handleOnChangeEvent = (item) => {
+    switch (item.target.value) {
+        case 'latest':
+            filterLatestMovies();
+            break;
+        case 'avengers':
+            filterMovies('Avengers');
+            break;
+        case 'xmen':
+            filterMovies('X-Men');
+            break;
+        case 'princess':
+            filterMovies('Princess');
+            break;
+        case 'batman':
+            filterMovies('Batman');
+            break;
+        default:
+            console.log('Wrong entry at movie filter!');
+    };
+};
+
+radioBtns.forEach((item => {
+    item.addEventListener('change', (item) => {
+        handleOnChangeEvent(item);
+    });
+}));
+
+const pageLoad = () => {
+    startList = moviesToImg(movies);
+    startList.forEach(item => {
+        addMoviesToDom(item);
+    })
+};
+
+pageLoad();
